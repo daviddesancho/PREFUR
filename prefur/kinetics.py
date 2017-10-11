c99de6e4baf58e8ad4ac1bc544ca97897aaf2004
David De Sancho <daviddesancho@gmail.com>
2017-10-11 13:33:52 +0200

    rate estimation

---
+++ prefur/kinetics.py
@@ -0,0 +1,22 @@
+# This code is part of PREFUR
+
+import numpy as np
+
+def rates(DDG=None, nres=None, temp=298):
+    """
+    Estimation of (un)folding rates in s^-1 from the free energy barrier
+
+    Parameters
+    ----------
+    barrier : float
+        Free energy barrier in kJ/mol
+
+    nres : int
+        The number of amino acids
+
+    T : float
+        The temperature of interest
+        
+    """
+    beta = 1/(8.314e-3 * temp)
+    return 3.6e6/nres*np.exp(-beta*barrier)
